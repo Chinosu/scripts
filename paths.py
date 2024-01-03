286c0e21f3230b66482654e293c4af2251c3aa9a
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-01-03 22:49:34 +1100

    Create hypertest-info subcommand

--- paths.py
+++ paths.py
@@ -3,3 +3,6 @@ from pathlib import Path
 
 def autotest_path():
     return Path(__file__).resolve().parent / 'submodules' / 'autotest' / 'autotest.py'
+
+def hypertest_module_path():
+    return Path(__file__).resolve().parent
